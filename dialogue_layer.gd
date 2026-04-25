9ff1eb5d787bfb544bc1cdd8c66b198dde49fe08
janicedtea <janicesu2019@gmail.com>
2026-04-25 17:17:31 -0400

    tilesets

--- dialogue_layer.gd
+++ dialogue_layer.gd
@@ -1,7 +1,4 @@
-extends Node2D
-
-
-
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,5 +7,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
---
+++ npc.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+#
+#@onready var player = get_parent().get_node("Player")
+#var player_in_range = false
+#
+#func _process(_delta):
+	#if player_in_range and Input.is_action_just_pressed("interact"):
+		#interaction()
+		#
+##func interact():
+##	var dialogue = preload("res://dialogue_layer.tscn").instantiate()
+##	get_tree().current_scene.add_child(dialogue)
+##	dialogue.start_dialogue(["uhhh my name's peppermint, who're you?", "um... okay... rude", "whatever"], "Peppermint", preload("res://npc.png"))
+##	await dialogue.tree_exited
+	#
+#
+#func _on_area_2d_body_entered(body):
+	#if body.name == "Player":
+		#player_in_range = true
+#
+#func _on_area_2d_body_exited(body):
+	#if body.name == "Player":
+		#player_in_range = false
+#
+#
+#func _on_area_2d_area_entered(area: Area2D) -> void:
+	#if area.name == "Player":
+		#player_in_range = true
+#
+#func _on_area_2d_area_exited(area: Area2D) -> void:
+	#if area.name == "Player":
+		#player_in_range = false
+		#
+#
+#func interaction():
+	#print("yayy")
